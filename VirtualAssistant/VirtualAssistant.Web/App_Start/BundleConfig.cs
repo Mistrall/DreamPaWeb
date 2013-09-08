8d5ea3fa6f0ab3f780e06af1a5ab8daf4c4f83a2
Tony G <mipt.shootnick.spam@gmail.com>
2013-09-09 00:55:29 +0400

    Move to clean canvas theme!
    Signed-off-by: Tony G <mipt.shootnick.spam@gmail.com>

--- VirtualAssistant/VirtualAssistant.Web/App_Start/BundleConfig.cs
+++ VirtualAssistant/VirtualAssistant.Web/App_Start/BundleConfig.cs
@@ -31,19 +31,28 @@ namespace VirtualAssistant.Web.App_Start
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery.ui.core.css",
-						"~/Content/themes/base/jquery.ui.resizable.css",
-						"~/Content/themes/base/jquery.ui.selectable.css",
-						"~/Content/themes/base/jquery.ui.accordion.css",
-						"~/Content/themes/base/jquery.ui.autocomplete.css",
-						"~/Content/themes/base/jquery.ui.button.css",
-						"~/Content/themes/base/jquery.ui.dialog.css",
-						"~/Content/themes/base/jquery.ui.slider.css",
-						"~/Content/themes/base/jquery.ui.tabs.css",
-						"~/Content/themes/base/jquery.ui.datepicker.css",
-						"~/Content/themes/base/jquery.ui.progressbar.css",
-						"~/Content/themes/base/jquery.ui.theme.css"));
+			bundles.Add(new StyleBundle("~/css/clean-canvas-theme")
+				.Include("~/Content/clean-canvas-bootstrap/bootstrap-overrides.css")
+				.Include("~/Content/clean-canvas-bootstrap/theme.css")
+				.Include("~/Content/clean-canvas-bootstrap/lib/animate.css"));
+
+			bundles.Add(new ScriptBundle("~/bundles/clean-canvas-js")
+				.Include("~/Content/clean-canvas-bootstrap/js/theme.js")
+				.Include("~/Content/clean-canvas-bootstrap/js/index-slider.js"));
+
+//			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+//						"~/Content/themes/base/jquery.ui.core.css",
+//						"~/Content/themes/base/jquery.ui.resizable.css",
+//						"~/Content/themes/base/jquery.ui.selectable.css",
+//						"~/Content/themes/base/jquery.ui.accordion.css",
+//						"~/Content/themes/base/jquery.ui.autocomplete.css",
+//						"~/Content/themes/base/jquery.ui.button.css",
+//						"~/Content/themes/base/jquery.ui.dialog.css",
+//						"~/Content/themes/base/jquery.ui.slider.css",
+//						"~/Content/themes/base/jquery.ui.tabs.css",
+//						"~/Content/themes/base/jquery.ui.datepicker.css",
+//						"~/Content/themes/base/jquery.ui.progressbar.css",
+//						"~/Content/themes/base/jquery.ui.theme.css"));
 		}
 	}
 }
--- VirtualAssistant/VirtualAssistant.Web/Controllers/HomeController.cs
+++ VirtualAssistant/VirtualAssistant.Web/Controllers/HomeController.cs
@@ -24,5 +24,10 @@ namespace VirtualAssistant.Web.Controllers
 
 			return View();
 		}
+
+		public ActionResult Pricing()
+		{
+			return View();
+		}
 	}
 }
